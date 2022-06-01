32c6dc5df970d4f6cfa7dc34a5e7174b77e453b6
ShrutiAher <shruti.aher@thoughtworks.com>
2022-06-01 15:13:52 +0530

    Added new features(Convert to lowercase, word, char count and preview)

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,9 +1,13 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
-  const [text, setText] = useState('Enter text here');
+  const [text, setText] = useState('');
   const handleUppercaseClick = () => {
     setText(text.toUpperCase());
+  }
+
+  const handleLowercaseClick = () => {
+    setText(text.toLowerCase());
   }
 
   const handleOnChange = (event) => {
@@ -11,12 +15,24 @@ export default function TextForm(props) {
   }
 
   return (
-    <div>
-       <h3> {props.heading} </h3> 
-<div className="mb-3">
-  <textarea className="form-control" id="myBox" onChange={handleOnChange} value={text} rows="8"></textarea>
-</div>
-<button className="btn btn-primary" onClick={handleUppercaseClick}> Convert to Uppercase </button>
+    <>
+      <div className='container'>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" id="myBox" onChange={handleOnChange} value={text} rows="8"></textarea>
+        </div>
+      <button className="btn btn-primary mx-2" onClick={handleUppercaseClick}> Convert to Uppercase </button>
+      <button className="btn btn-primary MX-2" onClick={handleLowercaseClick}> Convert to Lowercase </button>
     </div> 
+
+
+    <div className="container my-3">
+      <h2>Your text summary</h2>
+      <p>{text.split(" ").length} words {text.length} characers</p>
+      <p>{0.008 * text.split(" ").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+  </>
   )
 }
