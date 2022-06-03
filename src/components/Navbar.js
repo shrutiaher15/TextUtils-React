fffd23b96f6ab4b2dfc17762afb0cea44ed683ca
ShrutiAher <shruti.aher@thoughtworks.com>
2022-06-03 21:26:33 +0530

    Refactor. README file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,10 +40,6 @@ export default function Navbar(props) {
             <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.mode === 'light' ? 'Dark' : 'Light'} Mode</label>
           </div>
-          {/* <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-primary" type="submit">Search</button>
-          </form> */}
         </div>
       </div>
     </nav>
