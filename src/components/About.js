a03c152d9b9b6928a68da1a007c308103e91da39
ShrutiAher <shruti.aher@thoughtworks.com>
2022-06-03 21:04:06 +0530

    Add. github repo. link in about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,7 @@
+import {
+    Link
+} from "react-router-dom";
+
 
 export default function About() {
     return (<>
@@ -10,11 +14,10 @@ export default function About() {
             <img src="/my-pic.jpeg" className="col-md-3 float-md-start mb-3 ms-md-3 mx-5" alt="about-us-page" />
 
             <h3 className="mx-10 my-3 mt-10">Hello, Shruti Aher here!</h3>
-            <i style={{ fontSize: '20px' }}>Application Developer at Thoughtworks</i>
             <div className="container my-5">
                 <ul>
                     <li style={{ marginBottom: '20px' }}>This project has been the part of my React learning journey.</li>
-                    <li style={{ marginBottom: '10px' }}>Github Link : </li>
+                    <li style={{ marginBottom: '10px' }}> Github Link :   <a href="https://github.com/shrutiaher15/TextUtils-React"> https://github.com/shrutiaher15/TextUtils-React </a></li>
                 </ul>
             </div >
         </div>
